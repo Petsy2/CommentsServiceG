113f0c536af02c2ab9151b2e670b1eeca986c50e
Gabriel Bodeen <gabebodeen@gmail.com>
2019-01-03 11:55:17 -0600

    Order reviews chronologically.

--- database/controllers.js
+++ database/controllers.js
@@ -2,7 +2,7 @@ const options = require('./knexfile');
 const knex = require('knex')(options.development); // FIXME: Shouldn't always use development.
 
 const getReviewsForPet = (pet_id) => {
-  return knex.select().from('reviews').where({ pet_id });
+  return knex.select().from('reviews').where({ pet_id }).orderBy('review_created');
 };
 
 const postReviewForPet = (pet_id, user_id, text) => {

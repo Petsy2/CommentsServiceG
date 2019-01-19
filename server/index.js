ca9daada1996d2ae74b63ad4570248767ccceb80
ericcchiu <eric.chn.chiu@gmail.com>
2019-01-18 19:11:07 -0600

    (feat) Add and use morgan middleware.

--- server/index.js
+++ server/index.js
@@ -3,14 +3,16 @@ const app = express();
 const router = require('./router.js');
 const bodyparser = require('body-parser');
 const cors = require('cors');
+const morgan = require('morgan');
+
 
 app.use(cors());
 app.use(bodyparser.json());
 app.use(express.static('public'));
 app.use(router);
-
+app.use(morgan('dev'));
 const port = process.env.PORT || 3003;
-const server = app.listen(port);
+const server = app.listen(port, console.log(`Server running on port ${port}`));
 
 module.exports = {
   app,

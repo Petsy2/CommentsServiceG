f09b24b018ed0011c739181fc25da710c23ffd4b
ericcchiu <eric.chn.chiu@gmail.com>
2019-01-22 20:00:19 -0600

    (feat) Set up server cluster for mongoDB.

---
+++ server/serverMongoDB/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const router = require('./router.js');
+const bodyparser = require('body-parser');
+const cors = require('cors');
+// const morgan = require('morgan');
+
+
+app.use(cors());
+app.use(bodyparser.json());
+app.use(express.static('public'));
+app.use(router);
+// app.use(morgan('dev'));
+const port = process.env.PORT || 5000;
+const server = app.listen(port, console.log(`Server running on port ${port}`));
+
+module.exports = {
+  app,
+  server
+};
